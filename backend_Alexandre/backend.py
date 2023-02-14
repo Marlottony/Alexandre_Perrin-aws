bebd9c7a501bc40107fd11dc60703e5e607b8b16
Marlottony <marlottony2@gmail.com>
2023-02-14 16:14:05 +0100

    GET

--- backend_Alexandre/backend.py
+++ backend_Alexandre/backend.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/yoyoyo')
-def hello():
-    return 'Hello World!'
+@app.route('/api/v1/employees', methods=['GET'])
+def get_employees():
+    employees = [
+        {'id': 1, 'firstName': 'Paul', 'lastName': 'Jack', 'emailId': 'pauljacke@example.com'},
+        {'id': 2, 'firstName': 'Janette', 'lastName': 'Ongle', 'emailId': 'janetteongle@example.com'},
+        {'id': 3, 'firstName': 'Phylippe', 'lastName': 'Munche', 'emailId': 'phylippemunche@example.com'}
+    ]
+    return jsonify(employees)
+
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(debug=True, port=8080)
+    
+
+
+    
+
+    
+    
+    
+    
