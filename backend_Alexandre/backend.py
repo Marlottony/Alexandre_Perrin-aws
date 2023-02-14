122c04d874cecfe3ac0c9f8ff2d97cb594590a77
Marlottony <marlottony2@gmail.com>
2023-02-14 16:16:12 +0100

    POST

--- backend_Alexandre/backend.py
+++ backend_Alexandre/backend.py
@@ -9,8 +9,18 @@ def get_employees():
         {'id': 2, 'firstName': 'Janette', 'lastName': 'Ongle', 'emailId': 'janetteongle@example.com'},
         {'id': 3, 'firstName': 'Phylippe', 'lastName': 'Munche', 'emailId': 'phylippemunche@example.com'}
     ]
+    
     return jsonify(employees)
-
+@app.route('/api/v1/employees', methods=['POST'])
+def create_employee():
+    new_employee = {
+        'id': request.json['id'],
+        'firstName': request.json['firstName'],
+        'lastName': request.json['lastName'],
+        'emailId': request.json['emailId']
+    }
+    employees.append(new_employee)
+    return jsonify(new_employee), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
