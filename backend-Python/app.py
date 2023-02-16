ec551cd9c47ee24fd7249c5fbd05b164bfcb4ed8
Marlottony <marlottony2@gmail.com>
2023-02-16 14:35:42 +0100

    modification

--- backend-Python/app.py
+++ backend-Python/app.py
@@ -1,53 +1,43 @@
-from flask import request
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
+employees = [
+        {'id': 1, 'firstName': 'Sanchez', 'lastName': 'Alexis', 'emailId': 'alxissanchez@example.com'},
+        {'id': 2, 'firstName': 'Dimitry', 'lastName': 'Payet', 'emailId': 'dimitrypayet@example.com'},
+        {'id': 3, 'firstName': 'Johnatan', 'lastName': 'Clauss', 'emailId': 'johnatanclauss@example.com'}
+    ]
+
 @app.route('/api/v1/employees', methods=['GET'])
 def get_employees():
-    employees = [
-        {'id': 1, 'firstName': 'Paul', 'lastName': 'Jack', 'emailId': 'pauljacke@example.com'},
-        {'id': 2, 'firstName': 'Janette', 'lastName': 'Ongle', 'emailId': 'janetteongle@example.com'},
-        {'id': 3, 'firstName': 'Phylippe', 'lastName': 'Munche', 'emailId': 'phylippemunche@example.com'}
-    ]
-    
     return jsonify(employees)
+
 @app.route('/api/v1/employees', methods=['POST'])
-def create_employee():
-    new_employee = {
-        'id': request.json['id'],
-        'firstName': request.json['firstName'],
-        'lastName': request.json['lastName'],
-        'emailId': request.json['emailId']
-    }
+def add_employee():
+    new_employee = request.json
+    new_employee['id'] = len(employees) + 1
     employees.append(new_employee)
     return jsonify(new_employee), 201
+
 
 @app.route('/api/v1/employees/<int:employee_id>', methods=['DELETE'])
 def delete_employee(employee_id):
     for employee in employees:
         if employee['id'] == employee_id:
             employees.remove(employee)
-            return jsonify({'message': f'Employee with ID {employee_id} has been deleted'})
+            return jsonify({'message': 'Employee has been deleted'}), 200
     return jsonify({'error': 'Employee not found'}), 404
 
 @app.route('/api/v1/employees/<int:employee_id>', methods=['PUT'])
 def update_employee(employee_id):
-    global employees
-    employee_to_update = None
     for employee in employees:
         if employee['id'] == employee_id:
-            employee_to_update = employee
-            break
-    if employee_to_update:
-        employee_data = request.get_json()
-        employee_to_update.update(employee_data)
-        return jsonify(employee_to_update)
-    else:
-        return jsonify({'erreur': f"L'employé avec l'ID {employee_id} n'a pas été trouvé"}), 404
+            employee.update(request.json)
+            return jsonify(employee), 200
+    return jsonify({'error': 'Employee not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True)
